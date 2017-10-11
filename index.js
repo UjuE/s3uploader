b7537b9fe65b2e844803e1f8c71a452d1e434fe1
Obianuju Vivian Ezeoke <none@none.com>
2017-10-11 14:08:03 +0100

    Still having problems

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require("electron")
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+    console.log(__dirname);
+
+    mainWindow.loadUrl(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
